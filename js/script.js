b0ba91a8c0d3f6b520bf7fd7f7defeb81e1b39b1
prutha28 <prutha.bits@gmail.com>
2017-04-05 11:45:43 -0400

    refactoring code.

--- js/script.js
+++ js/script.js
@@ -11,24 +11,44 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // code to load streetview
-
     var streetStr = $("#street").val();
     var cityStr = $("#city").val();
     var locationStr = streetStr + ", " + cityStr;
     // console.log(locationStr);
-    var url = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + locationStr + '';
-    var str = '<img class="bgimg" ' + 'src="' + url + '"' + '>' + '';
+    var cityStrEncoded = encodeURI(cityStr);
+
+    loadGoogleImages(locationStr);
+    loadNYTimes(locationStr);
+    loadWikiArticles(cityStr);
+    loadWeatherDetails();
+    loadEventBriteDetails();
+    return false;
+};
+
+
+// Code to Load the Google Street View Images
+var loadGoogleImages = function(locationStr){
+
+    // URL
+    var google_url = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + locationStr + '';
+    var str = '<img class="bgimg" ' + 'src="' + google_url + '"' + '>' + '';
     $body.append(str);
 
-    // code to load nytimes articles
-    var key = "*****************************";
+}
+
+// Code to Load the NYTimes Articles
+var loadNYTimes = function(locationStr){
+
+    // API Key needed to call the API methods
+    var nyt_key = '';
     var nyt_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+  
     var parameters = {
-        'api-key' : key,
+        'api-key' : nyt_key,
         'query' : locationStr
     };
 
+    // Using Jquery's AJAX method to make the API calls
     $.ajax({
         url : nyt_url,
         // url : nyt_url + 'abc', // code to deliberately mess the url
@@ -53,30 +73,21 @@ function loadData() {
         console.log('The newyork times artciles could not be loaded.');
         $('.nytimes-container').append('<p>The newyork times articles could not be loaded.</p>');
     });
+}
 
 
 
-    // Code to load wikipedia articles
-    // 1. Using CORS Headers
-    var wiki_url = 'https://www.mediawiki.org/w/api.php';
-    var cityStrEncoded = encodeURI(cityStr);
-    var headers = {
-        'origin' :'http://152.7.224.3:80/',
-        'user-agent' : 'prutha.bits@gmail.com'
-    };
+// Code to load wikipedia articles
+var loadWikiArticles = function(cityStrEncoded){
 
-    var wiki_params = {
-        'action' : 'query',
-        'format' : 'json',
-        'titles' : cityStrEncoded,
-        'prop' : 'revisions',
-        'rvprop' : 'content',
-        'headers' : headers
-    };
-
+    var wiki_url = 'https://www.mediawiki.org/w/api.php?format=json&callback=wikiCallback&action=opensearch&search=' 
+                    + cityStrEncoded ;
+    
+    // Accessing Wikipedia articles lead to CORS, so we handle the CORS requests using the HTTP headers and JSONP
+    
+    // 1. Using CORS Headers    
     // $.ajax({
     //     url : wiki_url,
-    //     data : wiki_params,
     //     success : function(result){
     //         console.log(result);
     //     }
@@ -93,21 +104,101 @@ function loadData() {
     //     'rvprop' : 'content'
     // };
 
-    var wiki_url_1 = 'https://www.mediawiki.org/w/api.php?format=json&callback=wikiCallback&action=opensearch&search=' + cityStrEncoded ;
-    console.log(wiki_url_1);
-    $.ajax({
-        url : wiki_url,
-        dataType : "jsonp",
-        headers : {
-            'content-type' : 'application/json',
-            'accept' : 'application/json'
-        },
-        success : function( result){
-            console.log(result);
-        }
-    });
+    // $.ajax({
+    //     url : wiki_url,
+    //     dataType : "jsonp",
+    //     headers : {
+    //         'content-type' : 'application/json',
+    //         'accept' : 'application/json'
+    //     },
+    //     success : function( result){
+    //         console.log(result);
+    //     }
+    // });
+}
 
-    return false;
-};
+
+// XHR Requests to weather API
+// Code to load weather details 
+var loadWeatherDetails = function(){
+
+     // Using the XHR request object with the weather api
+    var hotelLocationStr = encodeURI('London,England');
+    var weather_url = 'http://api.openweathermap.org/data/2.5/' ;
+    var weatherApiKey = '';
+    weather_url += 'weather?q=' + hotelLocationStr + '&appid=' + weatherApiKey ;
+    var xhrRequest ;
+
+    if( window.XMLHttpRequest){
+        xhrRequest = new XMLHttpRequest();
+    }else{
+        xhrRequest = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+    var handleResponse = function(){
+
+        // This code is reached everytime there is a change in the readyState (1,2,3,4)
+        // console.log('State :' + xhrRequest.readyState);
+       
+        if( xhrRequest.readyState === 4) {
+            if( xhrRequest.status === 200){
+                handleSuccess(xhrRequest.responseText);
+            }else{
+                handleFailure();
+            }
+        }
+    };
+
+    xhrRequest.onreadystatechange = handleResponse;
+    xhrRequest.open('GET', weather_url, true);
+    xhrRequest.send();
+}
+
+
+// Handle XHR Success
+var handleSuccess = function(responseText){
+    // console.log('XHR Response :');
+    // console.log(xhrRequest.response);
+
+    var json_response = JSON.parse(responseText);
+
+    // API gives temperatures in Kelvin
+    var temp_max = json_response.main.temp_max;
+    var temp_min = json_response.main.temp_min;
+
+    // Convert these to celsius
+    var temp_max_c = Math.floor(temp_max - 273.15) ;
+    var temp_min_c = Math.floor(temp_min - 273.15) ;
+
+
+    // console.log(max_temp_f);
+    // console.log(min_temp_f);
+
+    var $weather_container = $('.wikipedia-container');
+    $weather_container.append('<div class="weather-report">');
+   
+    $weather_container.append('<p>');
+    $weather_container.append('Minimum Temperature is ' + temp_min_c);
+    $weather_container.append('</p>');
+    $weather_container.append('<p>');
+    $weather_container.append('Maximum Temperature is ' + temp_max_c);
+    $weather_container.append('</p>');
+    $weather_container.append('</div>');
+}
+
+// Handle XHR error
+var handleFailure = function(){
+    console.log('Something went wrong!');
+}
+
+
+var loadEventBriteDetails = function(){
+    
+    // Event Brite API key needed
+    var eventBriteApiKey = '';
+    
+
+}
+
 
 $('#form-container').submit(loadData);
